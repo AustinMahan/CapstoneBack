2e8a83d7f7716d7a12709db2175f57b0ca7689bb
AustinMahan <amahan0424@gmail.com>
2016-12-02 13:17:02 -0700

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,17 @@ const databaseName = 'unit_4';
 module.exports = {
   development: {
     client: 'postgresql',
-    connection: `postgres://localhost:5432/${databaseName}`,
+    connection: process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
